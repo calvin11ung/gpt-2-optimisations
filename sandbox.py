f2de734dc5e2566da8f45c68a0d4b3e7b51b7b98
calvin11ung <calvin11ung@gmail.com>
2024-04-07 20:09:43 +1000

    polished file

--- sandbox.py
+++ sandbox.py
@@ -4,8 +4,7 @@ from transformers.generation.logits_process import TopPLogitsWarper
 import torch
 import os
 from datetime import datetime, timedelta
-import onnxruntime
-import numpy
+import numpy as np
 from onnxruntime.transformers.io_binding_helper import TypeHelper
 from onnxruntime.transformers.io_binding_helper import IOBindingHelper
 import torch.nn.functional as F
@@ -13,6 +12,7 @@ from torch.nn import CrossEntropyLoss
 from evaluate import logging
 import json
 from ctransformers import AutoModelForCausalLM
+import argparse
 
 def get_tokenizer(model_name_or_path, cache_dir):
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, cache_dir=cache_dir)
@@ -47,57 +47,6 @@ def get_example_inputs(prompt_text, tokenizer, num_attention_heads, hidden_size,
 
     return input_ids, attention_mask, position_ids, empty_past
 
-def inference_with_io_binding(session, config, input_ids, position_ids, attention_mask, past):
-    output_shapes = Gpt2Helper.get_output_shapes(
-        batch_size=input_ids.size(0),
-        past_sequence_length=past[0].size(3),
-        sequence_length=input_ids.size(1),
-        config=config,
-    )
-    output_buffers = Gpt2Helper.get_output_buffers(output_shapes, 'cpu')
-
-    io_binding = IOBindingHelper.prepare_io_binding(
-        session, input_ids, position_ids, attention_mask, past, output_buffers, output_shapes
-    )
-    session.run_with_iobinding(io_binding)
-
-    outputs = Gpt2Helper.get_outputs_from_io_binding_buffer(session, output_buffers, output_shapes, return_numpy=False)
-    return outputs
-
-def greedy(next_token_logits):
-
-    next_tokens = torch.argmax(next_token_logits, dim=-1)
-
-    return next_tokens
-
-def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')):
-    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
-        Args:
-            logits: logits distribution shape (vocabulary size)
-            top_k >0: keep only top k tokens with highest probability (top-k filtering).
-            top_p >0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
-                Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
-    """
-    assert logits.dim() == 1  # batch size 1 for now - could be updated for more but the code would be less clear
-    top_k = min(top_k, logits.size(-1))  # Safety check
-    if top_k > 0:
-        # Remove all tokens with a probability less than the last token of the top-k
-        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
-        logits[indices_to_remove] = filter_value
-
-    if top_p > 0.0:
-        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
-
-        # Remove tokens with cumulative probability above the threshold
-        sorted_indices_to_remove = cumulative_probs > top_p
-        # Shift the indices to the right to keep also the first token above the threshold
-        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
-        sorted_indices_to_remove[..., 0] = 0
-
-        indices_to_remove = sorted_indices[sorted_indices_to_remove]
-        logits[indices_to_remove] = filter_value
-    return logits
 
 def compute_perplexity(
         data, model, tokenizer, batch_size: int = 16, add_start_token: bool = True, device=None, max_length=None
@@ -184,20 +133,14 @@ def test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, 
 
     device = 'cpu'
 
-    print("Text generation using PyTorch")
     eos_token_id = tokenizer.eos_token_id
 
     batch_size = input_ids.size(0)
-
-    print(batch_size)
 
     has_eos = torch.zeros(batch_size, dtype=torch.bool)
 
     all_token_ids = input_ids.clone()
 
-    ppl = []
-
-    loss_fct = torch.nn.CrossEntropyLoss(reduction='none')
 
     for step in range(num_tokens_to_produce):
         with torch.no_grad():
@@ -222,12 +165,12 @@ def test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, 
 
         past = list(outputs[1])
 
-        # if torch.all(has_eos):
-        #     break
+        if torch.all(has_eos):
+            break
 
     sequence = ""
 
-    for i, output in enumerate(all_token_ids):
+    for output in all_token_ids:
         sequence += tokenizer.decode(output, skip_special_tokens=True)
     print(sequence)
 
@@ -263,159 +206,189 @@ def test_generation_ggml(tokenizer, input_ids, ggml_model, logits_warper, num_to
         all_token_ids = torch.cat([all_token_ids, tokens_to_add], dim=-1)
 
         input_ids = tokens_to_add.clone().detach().reshape([batch_size, 1]).to(device)
-        # if torch.all(has_eos):
-        #     break
+        if torch.all(has_eos):
+            break
 
     sequence = ""
 
-    for i, output in enumerate(all_token_ids):
+    for output in all_token_ids:
         sequence += tokenizer.decode(output, skip_special_tokens=True)
 
     print(sequence)
 
+def load_32_bit_model():
 
-def main():
-    torch.set_num_threads(10)
-    torch.manual_seed(42)
-    device = torch.device("cpu")
-
-    sample_text = '/home/calvin/gpt2/data/gpt-2-output-dataset/data/webtext.test.jsonl'
-
-    input_text_samples = []
-    bucket = []
-    batch_size = 1
+    model_name_or_path = "gpt2-xl"
 
     cache_dir = os.path.join(".", "cache_models")
     if not os.path.exists(cache_dir):
         os.makedirs(cache_dir)
-    
-    model_name_or_path = "gpt2-xl"
-    # model_name_or_path = "gpt2-quantized-2.pt"
-    quant_model = torch.load("gpt2-xl-quantized.pt")
-    quant_model.eval().to(device)
-    
+
     config = AutoConfig.from_pretrained("gpt2-xl", cache_dir=cache_dir)
     model = GPT2LMHeadModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir)
 
-    num_attention_heads = model.config.n_head
-    hidden_size = model.config.n_embd
-    num_layer = model.config.n_layer
+    return model
 
-    tokenizer = get_tokenizer("gpt2-xl", cache_dir)
+def load_8_bit_model():
 
-    # First, read all lines and sort them by length
+    model = torch.load("gpt2-xl-quantized.pt")
+    model.eval().to('cpu')
+
+    return model
+
+def load_4_bit_model():
+
+    model = AutoModelForCausalLM.from_pretrained('/home/calvin/gpt2/ggml/build/models/gpt-2-1558M/ggml-model-q4_0.bin', model_type='gpt2')
+
+    return model
+
+def load_text(batch_size):
+
+    sample_text = '~/gpt2-optimisation/data/gpt-2-output-dataset/data/webtext.test.jsonl'
+
     lines = []
 
     with open(sample_text, 'r') as f:
         for line in f:
             lines.append(json.loads(line)['text'])
 
-    sorted_lines = sorted(lines, key=len)
+    return sorted(lines, key=len)    
 
-    ggml_text_samples = []
 
-    # Now, group the sorted lines into sublists of 20
-    for text in sorted_lines:
-        bucket.append(text)
-        if len(bucket) == batch_size:
-            input_text_samples.append(bucket)
-            bucket = []
-        ggml_text_samples.append(get_ggml_example_inputs(text, tokenizer, num_attention_heads, hidden_size, num_layer, device))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--benchmark', action='store_true', help='Run benchmarking')
+    parser.add_argument('-p', '--perplexity', action='store_true', help='Calculate perplexity')
+    parser.add_argument('-n', '--num_batches', type=int, help='Number of batches to test on')
+    parser.add_argument('-bsz', '--batch_size', type=int, default=16, help='Number of samples to run batched inference on')
 
-    # Check for any remaining items in bucket not yet added to input_text_samples
-    if bucket:
-        input_text_samples.append(bucket)
+    args = parser.parse_args()
 
-    computed_batches = []
+    torch.set_num_threads(10)
+    torch.manual_seed(42)
 
-    for batch in input_text_samples:
+    device = torch.device("cpu")
 
-        computed_batches.append(get_example_inputs(batch, tokenizer, num_attention_heads, hidden_size, num_layer, device))
+    # model setup
 
-    ggml_model = AutoModelForCausalLM.from_pretrained('/home/calvin/gpt2/ggml/build/models/gpt-2-1558M/ggml-model-q4_0.bin', model_type='gpt2')
-    ggml_model.config.max_new_tokens = 30
+    model_32_bit = load_32_bit_model()
+
+    model_8_bit = load_8_bit_model()
+
+    model_4_bit = load_4_bit_model()
+
+    config = model_32_bit.config
+
+    cache_dir = os.path.join(".", "cache_models")
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+    
+    tokenizer = get_tokenizer("gpt2-xl", cache_dir)
+
+    # data loading setup
+
+    input_text_samples = load_text(args.batch_size)
+
+    # tokenise inputs 
+
+    encoded_batches = []
+    encoded_sequences = []
+
+    cur_batch = []
+
+    for idx, sample in enumerate(input_text_samples):
+        
+        encoded_sequences.append(get_example_inputs(sample, tokenizer, model_32_bit.config.n_head, model_32_bit.config.n_embd, model_32_bit.config.n_layer, device))
+
+        cur_batch.append(sample)
+
+        if idx == args.batch_size:
+
+            encoded_batches.append(get_example_inputs(cur_batch, tokenizer, model_32_bit.config.n_head, model_32_bit.config.n_embd, model_32_bit.config.n_layer, device))
+
+            cur_batch = []
+
+    # setup top-p sampling
+
     top_p_warper = TopPLogitsWarper(top_p=0.9)
 
+    if args.benchmark:
 
-    input_ids, attention_mask, position_ids, empty_past = computed_batches[0]
+        # Run batched benchmarking on fp32 and uint8 models
 
-    start = datetime.now()
-    output = test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, quant_model, top_p_warper)
-    print(f'pytorch uint8 took {datetime.now() - start}')
+        print("Starting batched benchmarks for PyTorch FP32")
 
-    start = datetime.now()
-    output = test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, model, top_p_warper)
-    print(f'pytorch fp32 took {datetime.now() - start}')
+        fp_32_batched_start = datetime.now()
 
-    start = datetime.now()
+        for idx in range(args.num_samples):
 
-    # test_generation_ggml(tokenizer, ggml_text_samples[0], ggml_model, top_p_warper)
-    all_token_ids = list(ggml_text_samples[0])
-    for idx, token in enumerate(ggml_model.generate(ggml_text_samples[0], top_p=0.9)):
-        all_token_ids.append(token)
-        if idx >= 30:
-            break
+            input_ids, attention_mask, position_ids, empty_past = encoded_batches[idx] 
 
-    sequence = ""
-
-    for i, output in enumerate(all_token_ids):
-        sequence += tokenizer.decode(output, skip_special_tokens=True)
-
-    print(sequence)
-    print(f'ggml took {datetime.now() - start}')
-
-    exit()
-
-    # breakpoint()
-    # for idx, input_ids in enumerate(ggml_text_samples):
-    #     start = datetime.now()
-
-    #     test_generation_ggml(tokenizer, input_ids, ggml_model, top_p_warper)
-    #     print(f'took {datetime.now() - start}')
-
-    # total_time = timedelta()
-    # perplexity = []
-
-    # for idx, (input_ids, attention_mask, position_ids, empty_past) in enumerate(computed_batches):
-
-    #     start = datetime.now()
-    #     test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, model, top_p_warper)
-    #     total_time += (datetime.now() - start)
-
-    #     perplexity.extend(compute_perplexity(model=model, tokenizer=tokenizer, add_start_token=False, data=input_text_samples[idx]))
-
-    #     if idx == 0:
-    #         break
-
-    # print(f'pytorch FP32 took {total_time}')
-    # print(f'mean perplexity: {numpy.mean(perplexity)}')
-
-    total_time = timedelta()
-    perplexity = []
-
-    for idx, (input_ids, attention_mask, position_ids, empty_past) in enumerate(computed_batches):
-
-        start = datetime.now()
-        test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, quant_model, top_p_warper)
-        total_time += (datetime.now() - start)
-
-        perplexity.extend(compute_perplexity(model=quant_model, tokenizer=tokenizer, add_start_token=False, data=input_text_samples[idx]))
-
-        if idx == 0:
-            break
-
-    print(f'pytorch UINT8 took {total_time}')
-    print(f'mean perplexity: {numpy.mean(perplexity)}')
-
-
-
-    # start = datetime.now()
-    # test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, quant_model, top_p_warper)
-    # print(f'pytorch uint8 took {datetime.now() - start}')
-    # results = compute_perplexity(model=quant_model, tokenizer=tokenizer, add_start_token=False, data=input_text_samples)
-    # print(f"UINT8 perplexity: {results}")
+            test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, model_32_bit, top_p_warper)
         
+        print(f'PyTorch FP32 took {datetime.now() - fp_32_batched_start} to complete {args.num_samples * args.batch_size} samples with batch size {args.batch_size}')
 
+        print("Starting batched benchmarks for PyTorch UINT8")
 
-if __name__ == '__main__':
-    main()
+        uint_8_batched_start = datetime.now()
+
+        for idx in range(args.num_samples):
+
+            input_ids, attention_mask, position_ids, empty_past = encoded_batches[idx] 
+
+            test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, model_8_bit, top_p_warper)
+        
+        print(f'PyTorch UINT8 took {datetime.now() - uint_8_batched_start} to complete {args.num_samples * args.batch_size} samples with batch size {args.batch_size}')
+
+        # Running single sequence for all models
+        print("Starting single sequence benchmarks for GGML UINT4")
+
+        uint_4_batched_start = datetime.now()
+
+        for idx in range(args.num_samples * args.batch_size):
+
+            input_ids, attention_mask, position_ids, empty_past = encoded_sequences[idx]
+
+            test_generation_ggml(tokenizer, input_ids, model_4_bit, top_p_warper)
+
+        print(f'GGML UINT4 single sequences took {datetime.now() - uint_4_batched_start} for {args.num_samples * args.batch_size} samples')
+
+        print("Starting single sequence benchmarks for PyTorch FP32")
+
+        uint_32_single_start = datetime.now()
+
+        for idx in range(args.num_samples * args.batch_size):
+
+            input_ids, attention_mask, position_ids, empty_past = encoded_sequences[idx]
+
+            test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, model_32_bit, top_p_warper)
+
+        print(f'PyTorch FP32 single sequence took {datetime.now() - uint_32_single_start} for {args.num_samples * args.batch_size} samples')
+
+        print("Starting single sequence benchmarks for PyTorch uint8")
+
+        uint_8_single_start = datetime.now()
+
+        for idx in range(args.num_samples * args.batch_size):
+
+            input_ids, attention_mask, position_ids, empty_past = encoded_sequences[idx]
+
+            test_generation_pytorch(tokenizer, input_ids, attention_mask, position_ids, empty_past, config, model_8_bit, top_p_warper)
+
+        print(f'PyTorch FP32 single sequence took {datetime.now() - uint_8_single_start} for {args.num_samples * args.batch_size} samples')
+
+    if args.perplexity:
+
+        perplexities_32_bit = []
+        perplexities_8_bit = []
+        perplexities_4_bit = []
+
+        for idx in range(args.num_samples * args.batch_size):
+
+            perplexities_32_bit.extend(compute_perplexity(model=model_32_bit, tokenizer=tokenizer, add_start_token=False, data=input_text_samples[idx]))
+            perplexities_8_bit.extend(compute_perplexity(model=model_8_bit, tokenizer=tokenizer, add_start_token=False, data=input_text_samples[idx]))
+            perplexities_4_bit.extend(compute_perplexity(model=model_4_bit, tokenizer=tokenizer, add_start_token=False, data=input_text_samples[idx]))
+
+        print(f"FP32 Model: Averaged Perplexity: {np.mean(perplexities_32_bit)}, Perplexity Scores: {perplexities_32_bit}")
+        print(f"UINT8 Model: Averaged Perplexity: {np.mean(perplexities_8_bit)}, Perplexity Scores: {perplexities_8_bit}")
+        print(f"UINT4 Model: Averaged Perplexity: {np.mean(perplexities_4_bit)}, Perplexity Scores: {perplexities_4_bit}")
